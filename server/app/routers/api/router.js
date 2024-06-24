359664ffd59ca97a569f57922397138103de60dc
AurelienChetot <aurelien.chetot@gmail.com>
2024-06-24 23:41:33 +0200

    init: controller/router

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -6,9 +6,33 @@ const router = express.Router();
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+const utilisateursRouter = require("./utilisateurs/router");
 
-router.use("/items", itemsRouter);
+router.use("/utilisateur", utilisateursRouter);
+
+const categoriesRouter = require("./categories/router");
+
+router.use("/categories", categoriesRouter);
+
+const souscategoriesRouter = require("./souscategories/router");
+
+router.use("/souscategories", souscategoriesRouter);
+
+const produitsRouter = require("./produits/router");
+
+router.use("/produits", produitsRouter);
+
+const imageaccueilsRouter = require("./imageaccueils/router");
+
+router.use("/imageaccueils", imageaccueilsRouter);
+
+const commandesRouter = require("./commandes/router");
+
+router.use("/commandes", commandesRouter);
+
+const articlescommandesRouter = require("./articlescommandes/router");
+
+router.use("/articlescommandes", articlescommandesRouter);
 
 /* ************************************************************************* */
 
