02644de1095ce37d037bdbeaf662deee36d703da
AurelienChetot <aurelien.chetot@gmail.com>
2024-06-25 20:46:07 +0200

    feat:BREAD souscategorieActions.js and router

--- server/app/controllers/souscategorieActions.js
+++ server/app/controllers/souscategorieActions.js
@@ -35,6 +35,25 @@ const read = async (req, res, next) => {
 };
 
 // The E of BREAD - Edit (Update) operation
+const edit = async (req, res, next) => {
+  const souscategorie = { ...req.body, id: req.params.id };
+
+  try {
+    // Fetch a specific item from the database based on the provided ID
+    await tables.souscategorie.update(souscategorie);
+
+    // If the item is not found, respond with HTTP 404 (Not Found)
+    // Otherwise, respond with the item in JSON format
+    if (souscategorie == null) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
 // This operation is not yet implemented
 
 // The A of BREAD - Add (Create) operation
@@ -55,13 +74,30 @@ const add = async (req, res, next) => {
 };
 
 // The D of BREAD - Destroy (Delete) operation
+const destroy = async (req, res, next) => {
+  try {
+    // Fetch a specific categorie from the database based on the provided ID
+    const souscategorie = await tables.souscategorie.delete(req.params.id);
+
+    // If the souscategorie is not found, respond with HTTP 404 (Not Found)
+    // Otherwise, respond with the souscategorie in JSON format
+    if (souscategorie == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(souscategorie);
+    }
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
 // This operation is not yet implemented
 
 // Ready to export the controller functions
 module.exports = {
   browse,
   read,
-  // edit,
+  edit,
   add,
-  // destroy,
+  destroy,
 };
