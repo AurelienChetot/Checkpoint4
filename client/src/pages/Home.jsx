e291cf5316bf29061ba71e5d53da05e7ca67b5a4
AurelienChetot <aurelien.chetot@gmail.com>
2024-06-29 02:21:55 +0200

    feat: Add carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,26 @@
 import { useLoaderData } from "react-router-dom";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Home() {
   const images = useLoaderData();
 
   return (
     <div className="home-container">
-      {images.map((image) => (
-        <div key={image.id}>
-          <img src={image.image_url} alt="img-accueil" />
-        </div>
-      ))}
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showArrows
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {images.map((image) => (
+          <div key={image.id}>
+            <img src={image.image_url} alt="img-accueil" />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
