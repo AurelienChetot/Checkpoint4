d551823054b13cfd878e0947e5a2fd1540f7b1e8
AurelienChetot <aurelien.chetot@gmail.com>
2024-07-17 10:47:41 +0200

    feat: login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,27 @@
+import LOGIN from "../assets/images/login.jpg";
+
 export default function Login() {
   return (
-    <div>
-      <div>Hello Login</div>
+    <div className="login-container">
+      <img className="login-img" src={LOGIN} alt="Login images" />
+      <div className="text-login-container">
+        <p className="text-login-style">Se connecter</p>
+        <div className="form-group">
+          <div className="mail-container">
+            <label htmlFor="username">Adresse mail</label>
+            <input type="email" placeholder="Email" />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="password-container">
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" placeholder="Mot de passe" />
+          </div>
+        </div>
+        <button className="login-button" type="submit">
+          Connexion
+        </button>
+      </div>
     </div>
   );
 }
